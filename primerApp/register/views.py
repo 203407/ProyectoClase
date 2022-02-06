091f667b59a30d7fedef6725e3b3c30703b06a9d
203407 <203407@ids.upchiapas.edu.mx>
2022-02-06 07:36:58 -0600

    pre register

--- primerApp/register/views.py
+++ primerApp/register/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import permissions
 
-# Create your views here.
+
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes =[permissions.AllowAny]
+    serializer_class = RegisterSerializer
