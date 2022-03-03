cc0feaabf476dd116d8bb4024cda5ae27c840bef
203407 <203407@ids.upchiapas.edu.mx>
2022-03-03 14:43:27 -0600

    version 2.0

--- primerApp/Profile/urls.py
+++ primerApp/Profile/urls.py
@@ -1,7 +1,6 @@
-from Profile.views import RegisterIdView,RegisterIdPView
+from Profile.views import RegisterIdView
 from django.urls import path, re_path
 
 urlpatterns = [    
-    re_path(r'^v1/profile/(?P<pk>\d+)$', RegisterIdView.as_view()),
-    re_path(r'^v2/profile/(?P<pk>\d+)$', RegisterIdPView.as_view())
+    re_path(r'^v1/profile/(?P<pk>\d+)$', RegisterIdView.as_view())
 ]
--- primerApp/Profile/views.py
+++ primerApp/Profile/views.py
@@ -11,7 +11,79 @@ from rest_framework.views import APIView
 import os
 # Create your views here.
 
- 
+class RegisterIdView(APIView):
+    
+
+    def get_objectU(self,pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return 404  
+    
+    def get_objectP(self,pk):
+        try:
+            return Profile.objects.get(user=pk)
+        except Profile.DoesNotExist:
+            return 404  
+     
+
+    def get(self,request,pk,format=None):
+        idResponse = self.get_objectU(pk)
+
+        if idResponse != 404:
+
+            serializer = RegisterSerializer(idResponse,context={"request":request})
+            idResponseP = self.get_objectP(pk)
+
+            if idResponseP != 404:
+                serializerP = ProfileSerializerRegister(idResponseP,context={"request":request})
+               
+                respuesta = json.dumps(serializer.data)
+                respuesta = json.loads(respuesta)
+                respuesta.update({"img_profile":serializerP.data.__getitem__("img_profile")})
+                
+                return Response(respuesta)
+            else:                                 
+                respuesta = json.dumps(serializer.data)                
+                respuesta = json.loads(respuesta)
+                respuesta.update( {"img_profile":0})
+                return Response(respuesta)
+
+        else:                        
+            return Response(status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self,request,pk,format=None):
+        idResponse = self.get_objectU(pk)
+        idResponseP = self.get_objectP(pk)             
+                       
+        if idResponse != 404 and idResponseP != 404:       
+            user = RegisterSerializer2(idResponse,data=request.data, context={"request":request}) 
+            profil = ProfileSerializerRegister(idResponseP,data=request.data, context={"request":request})                                    
+                        
+            if profil.is_valid() and user.is_valid():                            
+
+                if "img_profile" in request.data:
+                    
+                    nameImg = str(idResponseP.img_profile).split("/")[1]                
+                    
+                    file_path = "assets/img_profile/"+nameImg
+                    if os.path.isfile(file_path):                                            
+                        os.remove(file_path)                       
+                    
+                user.save()                  
+                profil.save()    
+                       
+                return Response(status.HTTP_200_OK)
+            else:     
+                return Response(status.HTTP_400_BAD_REQUEST)
+        else:            
+            return Response("Id no encontrado",status.HTTP_400_BAD_REQUEST)
+        
+
+
+
+""" 
 class RegisterIdView(APIView):
     
 
@@ -98,5 +170,5 @@ class RegisterIdPView(APIView):
             else:                
                 return Response(profil.errors)
         else:            
-            return Response("Id no encontrado",status.HTTP_400_BAD_REQUEST)
+            return Response("Id no encontrado",status.HTTP_400_BAD_REQUEST) """
          
